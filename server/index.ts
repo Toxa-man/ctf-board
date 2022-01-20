27dd1d9950104dece854473e871ef4bb92b4c97f
Anton Kvyatkovsky <anton.kvyatkovsky@nordigy.ru>
2022-01-20 03:32:44 +0300

    Added build

--- server/index.ts
+++ server/index.ts
@@ -39,9 +39,9 @@ async function main() {
     const mainRouter = configRoutes();
     const app = express();
     if (process.env.NODE_ENV === 'production') {
-        mainRouter.use(express.static(path.join(__dirname, '../', 'client_static')));
+        mainRouter.use(express.static(path.join(__dirname, '../', 'client_static', 'build')));
         mainRouter.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, '../', 'client_static', 'index.html'))
+            res.sendFile(path.resolve(__dirname, '../', 'client_static', 'build', 'index.html'))
         });
         app.use(subdomain('ctf', mainRouter));
     }
